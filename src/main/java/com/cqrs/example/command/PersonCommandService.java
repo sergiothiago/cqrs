e6005ea6caca75fd9b2cb7003235dde0fd02c633
sergio.mendonca <stmfmendonca@rd.com.br>
2024-09-05 23:26:18 -0300

    Command Service CQRS

--- src/main/java/com/cqrs/example/command/PersonCommandService.java
+++ src/main/java/com/cqrs/example/command/PersonCommandService.java
@@ -2,6 +2,7 @@ package com.cqrs.example.command;
 
 import com.cqrs.example.exceptions.ResourceNotFoundException;
 import com.cqrs.example.model.Person;
+import com.cqrs.example.query.PersonQueryService;
 import com.cqrs.example.repository.PersonRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,14 +15,29 @@ import java.util.List;
 @Service
 public class PersonCommandService {
 
+    private static final Logger logger = LoggerFactory.getLogger(PersonCommandService.class);
+
     @Autowired
     private PersonRepository personRepository;
 
-    private static final Logger logger = LoggerFactory.getLogger(PersonCommandService.class);
+    @Autowired
+    private PersonQueryService personQueryService;
 
-//    public Person save(Person person){
-//        logger.info("Search All");
-//        return personRepository.findAll();
-//    }
+    public Person save(Person person){
+        logger.info("Save person");
+        return personRepository.save(person);
+    }
+
+    public Person update(Person person) throws ResourceNotFoundException {
+        personQueryService.findById(person.getId());
+        logger.info("Save person");
+        return personRepository.save(person);
+    }
+
+    public void delete(Long id) throws ResourceNotFoundException {
+        personQueryService.findById(id);
+        logger.info("delete person" + id);
+        personRepository.deleteById(id);
+    }
 
 }
