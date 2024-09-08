451ee9b0bfa6e30f0683509f82d22574ef270644
sergio.mendonca <stmfmendonca@rd.com.br>
2024-09-08 15:05:57 -0300

    Controlador do CQRS

--- src/main/java/com/cqrs/example/command/PersonCommandServiceImpl.java
+++ src/main/java/com/cqrs/example/command/PersonCommandServiceImpl.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -21,17 +22,20 @@ public class PersonCommandServiceImpl implements PersonCommandService{
     @Autowired
     private PersonQueryServiceImpl personQueryServiceImpl;
 
+    @Transactional
     public Person save(Person person){
         logger.info("Save person");
         return personRepository.save(person);
     }
 
+    @Transactional
     public Person update(Person person) throws ResourceNotFoundException {
         personQueryServiceImpl.findById(person.getId());
         logger.info("Save person");
         return personRepository.save(person);
     }
 
+    @Transactional
     public void delete(Long id) throws ResourceNotFoundException {
         personQueryServiceImpl.findById(id);
         logger.info("delete person" + id);
--- src/main/java/com/cqrs/example/query/PersonQueryController.java
+++ src/main/java/com/cqrs/example/query/PersonQueryController.java
@@ -6,6 +6,7 @@ import com.cqrs.example.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,13 +25,16 @@ public class PersonQueryController {
     @Autowired
     private PersonQueryService personQueryService;
 
+
     @GetMapping
+    @Transactional(readOnly = true)
     public ResponseEntity<List<Person>> findAll() {
         List<Person> persons = personQueryService.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(persons);
     }
 
     @GetMapping("/{id}")
+    @Transactional(readOnly = true)
     public ResponseEntity<Person> findAll(@PathVariable("id") Long id) throws ResourceNotFoundException {
         Person person = personQueryService.findById(id);
         return ResponseEntity.status(HttpStatus.OK).body(person);
